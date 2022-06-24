35ac93d9db0113aab43ec6727355711398afed5a
skdysla <skdysla@gmail.com>
2022-06-24 11:43:36 +0900

    commit

---
+++ check_neutering.sql
@@ -0,0 +1,8 @@
+-- 중성화 여부 파악하기
+SELECT ANIMAL_ID, NAME,
+CASE WHEN INSTR(SEX_UPON_INTAKE, 'Neutered') > 0 THEN 'O'
+     WHEN INSTR(SEX_UPON_INTAKE, 'Spayed') > 0 THEN 'O'
+     ELSE 'X'
+     END AS "중성화"
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
